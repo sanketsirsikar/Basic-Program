17abad1c53cd180aba8b6b499cc3fe17c27da5e6
sanketsirsikar <sanketsirsikar2@gmail.com>
2022-10-12 20:51:32 +0530

    Check Number even or odd

---
+++ src/com/Bridgelabz/Basic/CheckEvenOddNum.java
@@ -0,0 +1,13 @@
+package com.Bridgelabz.Basic;
+
+public class CheckEvenOddNum {
+    public static void main (String[]args){
+        int number = 10;
+        if (number % 2 == 0){
+            System.out.println( number +" is Even number");
+        }else {
+            System.out.println(number + " is Odd number");
+        }
+    }
+}
+
