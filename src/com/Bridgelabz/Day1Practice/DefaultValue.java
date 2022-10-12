3b23eca6b5046a9ed2e7458d7c074ba7209ef969
sanketsirsikar <sanketsirsikar2@gmail.com>
2022-10-12 21:06:49 +0530

    print Default Value

---
+++ src/com/Bridgelabz/Day1Practice/DefaultValue.java
@@ -0,0 +1,21 @@
+package com.Bridgelabz.Day1Practice;
+
+public class DefaultValue {
+    static boolean b;
+    static double d;
+    static float f;
+    static int i;
+    static long l;
+    static String s;
+
+    public static void main(String[] args) {
+        System.out.println("display the default value of all primitive data types");
+        System.out.println("boolean = " + b);
+        System.out.println("double = " + d);
+        System.out.println("float = " + f);
+        System.out.println("int = " + i);
+        System.out.println("long = " + l);
+        System.out.println("String = " + s);
+    }
+}
+// default value
