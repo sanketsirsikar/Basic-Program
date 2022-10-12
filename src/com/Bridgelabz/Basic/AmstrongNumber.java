4e49f00c7c5f3d707b1a009bdc36c3bdf2c4844f
sanketsirsikar <sanketsirsikar2@gmail.com>
2022-10-12 20:39:03 +0530

    Number is Amstrong or Not

--- src/com/Bridgelabz/Basic/AmstrongNumber.java
+++ src/com/Bridgelabz/Basic/AmstrongNumber.java
@@ -1,2 +1,25 @@
-package com.Bridgelabz.Basic;public class AmstrongNumber {
+package com.Bridgelabz.Basic;
+
+public class AmstrongNumber {
+    public static void ArmStrongNum() {
+        boolean isArmStrong = true;
+        int num=154;
+        int temp = num;
+        int r;
+        int sum = 0;
+        while (num > 0) {
+            r = num % 10;
+            num = num / 10;
+            sum = sum + r * r * r;
+        }
+        if (temp == sum) {
+            System.out.println("its armstrong number");
+        } else {
+            System.out.println("not arm strong number");
+        }
+    }
+
+    public static void main(String[] args) {
+        ArmStrongNum();
+    }
 }
