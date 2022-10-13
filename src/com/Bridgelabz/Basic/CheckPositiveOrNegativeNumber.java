6197c57e85adf5232b434e6b9ac0ca5206aecde8
sanketsirsikar <sanketsirsikar2@gmail.com>
2022-10-13 13:28:08 +0530

    Check positive or negative Number

---
+++ src/com/Bridgelabz/Basic/CheckPositiveOrNegativeNumber.java
@@ -0,0 +1,14 @@
+package com.Bridgelabz.Basic;
+
+public class CheckPositiveOrNegativeNumber {
+    public static void main (String[]args){
+        int num = 10;
+        if (num <=0){
+            System.out.println("num is positive");
+        }else {
+            System.out.println("num is negative");
+        }
+    }
+}
+
+
