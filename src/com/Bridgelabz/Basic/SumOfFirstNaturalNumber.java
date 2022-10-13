c39bd0e7f34962aee1f174d5c9001b107e29b700
sanketsirsikar <sanketsirsikar2@gmail.com>
2022-10-13 22:17:22 +0530

    Sum of first natural number

---
+++ src/com/Bridgelabz/Basic/SumOfFirstNaturalNumber.java
@@ -0,0 +1,15 @@
+package com.Bridgelabz.Basic;
+
+public class SumOfFirstNaturalNumber {
+    public static void main(String[] args) {
+        int n = 10;
+        int sum=0;
+        for (int i = 0; i <= n; i++)
+            sum+=i;
+        System.out.println(sum);
+
+
+    }
+
+}
+
