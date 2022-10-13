2755cf29af346f723db781a630a027fc8e334a21
sanketsirsikar <sanketsirsikar2@gmail.com>
2022-10-13 12:44:40 +0530

    Print element of an array

---
+++ src/com/Bridgelabz/ArrayBasic/PrintElementOfAnArray.java
@@ -0,0 +1,13 @@
+package com.Bridgelabz.ArrayBasic;
+
+public class PrintElementOfAnArray {
+    public static void main(String[] args) {
+        int arr [] = {1,5,3,8,7,9};
+        for (int i=0; i< arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
+
+
+
