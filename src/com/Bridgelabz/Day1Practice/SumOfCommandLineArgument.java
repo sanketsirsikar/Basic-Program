8815cee75a8f15dcb85d389e900fbafed78e82c9
sanketsirsikar <sanketsirsikar2@gmail.com>
2022-10-13 10:03:26 +0530

    Sum of command line argument

--- src/com/Bridgelabz/Day1Practice/SumOfCommandLineArgument.java
+++ src/com/Bridgelabz/Day1Practice/SumOfCommandLineArgument.java
@@ -1,2 +1,11 @@
-package com.Bridgelabz.Day1Practice;public class SumOfCommandLineArgument {
+package com.Bridgelabz.Day1Practice;
+
+public class SumOfCommandLineArgument {
+    public static void main(String[] args) {
+        int A = Integer.parseInt(args[0]);
+        int B = Integer.parseInt(args[1]);
+        int sum = A+B;
+        System.out.println("sum of A and B is "+ sum);
+
+    }
 }
