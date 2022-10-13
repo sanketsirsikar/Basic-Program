c5e8f74d7ef41fa39852c75148a6d2286ae0af6d
sanketsirsikar <sanketsirsikar2@gmail.com>
2022-10-13 22:21:11 +0530

    Sum of natural number

---
+++ src/com/Bridgelabz/Basic/SumOfNaturalNumber.java
@@ -0,0 +1,12 @@
+package com.Bridgelabz.Basic;
+
+public class SumOfNaturalNumber {
+    public static void main (String[]args){
+        int n=6;
+        int sum=0;
+        for (int i=0; i<=n; i++)
+            sum+=i;
+        System.out.println(sum);
+
+    }
+}
