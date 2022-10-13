a4073f195e9aad3be08c274fa0f9192c379d4f47
sanketsirsikar <sanketsirsikar2@gmail.com>
2022-10-13 22:24:13 +0530

    Sum of number in given range

---
+++ src/com/Bridgelabz/Basic/SumOfNumberInGivenRange.java
@@ -0,0 +1,13 @@
+package com.Bridgelabz.Basic;
+
+public class SumOfNumberInGivenRange {
+    public static void main (String[]args){
+        int a =10;
+        int b = 17;
+        int sum =0;
+
+        for (int i=a; i<=b; i++)
+            sum+=i;
+        System.out.println(sum);
+    }
+}
